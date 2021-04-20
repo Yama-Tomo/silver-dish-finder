141602e3631e18c5b8e8c2e2ae89acff74f7af4f
Yama-Tomo <mazda13bturbo@gmail.com>
2021-04-20 12:27:12 +0900

    1st commit

---
+++ src/content_scripts.ts
@@ -0,0 +1,3 @@
+console.log('Hello from content script!');
+
+export const a = 1;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const entry = ['content_scripts', 'popup'].reduce(
+  (result, name) => ({ ...result, [name]: path.join(__dirname, 'src', name) }),
+  {}
+);
+
+module.exports = {
+  entry,
+  output: { path: path.join(__dirname, 'dist'), filename: '[name].js' },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: { loader: 'ts-loader', options: { transpileOnly: true } },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json', '.mjs', '.wasm'],
+  },
+  plugins: [new CopyWebpackPlugin({ patterns: [{ from: 'assets', to: '.' }] })],
+};
